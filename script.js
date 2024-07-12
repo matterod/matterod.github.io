8d5bc505cb7574a18099537f79c32286e60c9041
matterod <139497031+matterod@users.noreply.github.com>
2024-07-11 21:28:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyANGLfDfRnsIfN3k-COWI22Y0bi8emK4Os",
+    authDomain: "esp32rinconada.firebaseapp.com",
+    databaseURL: "https://esp32rinconada-default-rtdb.firebaseio.com",
+    projectId: "esp32rinconada",
+    storageBucket: "esp32rinconada.appspot.com",
+    messagingSenderId: "82707406557",
+    appId: "1:82707406557:web:62f5993a30a39b7f130534",
+    measurementId: "G-84QEWN29ZH"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
 $(document).ready(function(){
     var database = firebase.database();
     var currentUser = null;
